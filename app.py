66075842511a4bff4dcdc2d6c69f61838133da0c
Stephen <stephenlemasney@gmail.com>
2017-12-09 12:05:45 +0000

    updating to latest 12th dec

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask, render_template, request, Response, make_response, send_file
-from database import update_db, view_db, drop_db, create_db, datetime_now, datetime_today, view_workout_legacy
+from database import DataBase, TimeStmpToday
+from plots import workout_plot, update_fig
+import cStringIO
 import datetime
 
 app = Flask(__name__)
+today = TimeStmpToday()
+db = DataBase()
 
 @app.route('/')
 def index():
@@ -10,39 +14,52 @@ def index():
 
 @app.route('/db_reset',methods = ["POST","GET"])
 def db_reset():
-	drop_db()
-	create_db()
-	return render_template('db_reset.html')
+	db.create_table()
+	return render_template('index.html')
 
 @app.route('/sample',methods = ["POST","GET"])
 def sample():
-	df = view_workout_legacy()
+	df = db.load_legacy()
 	return render_template('legacy_results.html',  table = df.to_html(classes="table-striped"), 
-		datetime_today = datetime_today(), datetime_now = datetime_now())
+		datetime_today = today.today_date(), datetime_now = today.webpage_time())
 
 @app.route("/to_csv", methods = ["POST","GET"])
 def getWorkoutCSV():
 	path = r"C:\Users\Stephen\Desktop\sp\flask\tyred\static\csv\workout_results.csv"
-	csv = view_db().to_csv(path, encoding='utf-8')
+	csv =db. load_df().to_csv(path, encoding='utf-8')
 	return send_file(path, as_attachment=True)
 
 @app.route('/result', methods = ["POST","GET"])
 def result():
 	request.method == "POST"
 	if not request.form['workout_date']:
-		workout_date = datetime.datetime.now().strftime("%d/%m/%y")
+		workout_date = today.today_date()
 	else:
 		workout_date = request.form['workout_date']
 	workout_id = "TestID_1001"
 	workout_type = request.form['workout_type']
-	visits = request.form['visits']
 	distance = request.form['distance']
-	entry_date = datetime.datetime.now().strftime("%d/%m/%y")
-	entry_time = datetime.datetime.now().strftime("%H:%M:%S")
-	update_db(workout_id, workout_date, workout_type, visits, distance, entry_date, entry_time)
-	df = view_db()
+	entry_date = today.today_date()
+	entry_time = today.today_time()
+	db.update_db(workout_id, workout_date, workout_type, distance, entry_date, entry_time)
+	df = db.load_df()
 	return render_template('workout_results.html',  table = df.to_html(classes="table-striped"), 
-		datetime_today = datetime_today(), datetime_now = datetime_now())
+		datetime_today = today.today_date(), datetime_now = today.webpage_time())
+
+@app.route('/plots')
+def plots():
+	update_fig()
+	return render_template('to_plot.html')
+
+@app.after_request
+def add_header(response):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
+    response.headers['Cache-Control'] = 'public, max-age=0'
+    return response
 
 if __name__ == '__main__':
 	app.debug = True
--- database.py
+++ database.py
@@ -1,59 +1,80 @@
 import datetime
 import pandas as pd
 import sqlite3
+import os
+import csv
 
-static = 'C:\\Users\\Stephen\\Desktop\\sp\\flask\\tyred\\static\\'
+class TimeStmpToday():
+    
+    """
+    Returns timestamps used in the application.
+    """
+
+    def __init__(self):
+        self.now = datetime.datetime.now()
+
+    def webpage_time(self):
+        return "This web page was generated at: " + self.now.strftime("%H:%M:%S")
+
+    def today_date(self):
+        return self.now.strftime("%d/%m/%y")
+
+    def today_time(self):
+        return self.now.strftime("%H:%M:%S")
+
+class DataBase():
+
+    """
+    Returns Database and tables used in the application.
+    """
+
+    def __init__ (self, database = "workout.db", table = "workouts"):
+        self.database = database
+        self.table = table
+
+    def create_table(self):
+        csv_file = os.getcwd()+ "\static\\" + "workout_data_clean.csv"
+        if os.path.isfile(csv_file):
+        	self.create_legacy()
+        else:
+        	con =  sqlite3.connect(self.database)
+        	c = con.cursor()
+        	c.execute("drop table if exists workouts")
+        	c.execute("""CREATE TABLE workouts (workout_id NUMBER, workout_date TEXT, workout_type TEXT, 
+        		distance NUMBER, entry_date TEXT, entry_time TEXT);""")
+        	con.commit()
+
+    def update_db(self, workout_id, workout_date, workout_type, distance, entry_date, entry_time):
+        con =  sqlite3.connect(self.database)
+        c = con.cursor()
+        c.execute("INSERT INTO workouts VALUES(?,?,?,?,?,?);", (workout_id, workout_date, workout_type, distance, entry_date, entry_time))
+        con.commit()
+
+    def load_df(self):
+        con = sqlite3.connect('workout.db')
+        df = pd.read_sql_query("SELECT * FROM workouts", con)
+        return df
+
+    def load_legacy(self):
+        con = sqlite3.connect('workout_legacy.db')
+        df = pd.read_sql_query("SELECT * FROM workouts", con)
+        return df
+
+    def create_legacy(self):
+    	csv_file = os.getcwd()+ "\static\\" + "workout_data_clean.csv"
+    	con =  sqlite3.connect(self.database)
+        c = con.cursor()
+        c.execute("drop table if exists workouts")
+        c.execute("""CREATE TABLE workouts (workout_id NUMBER, workout_date TEXT, workout_type TEXT, 
+        	distance NUMBER, entry_date TEXT, entry_time TEXT);""")
+        con.commit()
+        with open(csv_file,'rb') as fin:
+        	dr = csv.DictReader(fin)
+        	to_db = [(i['workout_id'], i['workout_date'],i['workout_type'],i['distance'],
+        		i['entry_date'],i['entry_time'],) for i in dr]
+        	c.executemany("""INSERT INTO workouts (workout_id, workout_date, workout_type, distance, entry_date, entry_time) 
+        		VALUES (?,?,?,?,?,?);""", to_db)
+		con.commit()
+		con.close()
 
 
-def datetime_now():
-    return "This web page was generated at: " + datetime.datetime.now().strftime("%H:%M:%S")
-
-def datetime_today():
-    return datetime.datetime.now().strftime("%d/%m/%y")
-
-def drop_db():
-    with sqlite3.connect("workouts.db") as db:
-        c = db.cursor()
-        c.execute("DROP TABLE workouts")
-    db.close()
-
-def create_db():
-    with sqlite3.connect("workouts.db") as db:
-        c = db.cursor()
-        c.execute("CREATE TABLE workouts(workout_id NUMBER, workout_date TEXT, workout_type TEXT, visits TEXT, distance NUMBER, entry_date TEXT, entry_time TEXT);")
-        db.commit()
-    db.close()
-
-def update_db(workout_id, workout_date, workout_type, visits, distance, entry_date, entry_time):
-    with sqlite3.connect("workouts.db") as db:
-        c = db.cursor()
-        c.execute("INSERT INTO workouts VALUES(?,?,?,?,?,?,?);", (workout_id, workout_date, workout_type, visits, distance, entry_date, entry_time))
-        db.commit()
-    db.close()
-
-def view_dbold():
-    with sqlite3.connect("workouts.db") as db:
-        c = db.cursor()
-        for row in c.execute('SELECT * FROM workouts'):
-            print row
-    db.close()
-
-def view_db():
-    cnx = sqlite3.connect('workouts.db')
-    df = pd.read_sql_query("SELECT * FROM workouts", cnx)
-    return df
-
-def view_workout_legacy():
-    cnx = sqlite3.connect('workout_legacy.db')
-    df = pd.read_sql_query("SELECT * FROM workouts", cnx)
-    return df
-
-#drop_db()
-#create_db()
-#update_db("cycle","London")
-#print view_workout_legacy()
-
-#path = r"C:\Users\Stephen\Desktop\sp\flask\tyred\static\csv\workout_results.csv"
-#csv = view_db().to_csv(path, encoding='utf-8')
-
-print static
